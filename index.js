3d89769e0b3638f013330d5a2049e6f7951c4ad7
Blanca Hernandez <blamca1029@gmail.com>
2019-04-05 15:56:01 -0700

    changed styling and added favicon

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
-const youtubeKey = "AIzaSyBJeBAM0hFlN4U8owRBqCyXxUD4QkZD2t0";
-const FOOD_URL = "https://api.edamam.com/search";
-const foodId = "7fd223ec";
-const foodKey = "817ee5945f6f33e30affd1cededc234a";
-const RESTAURANT_URL = "https://api.foursquare.com/v2/venues/explore";
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
+const youtubeKey = 'AIzaSyBJeBAM0hFlN4U8owRBqCyXxUD4QkZD2t0';
+const FOOD_URL = 'https://api.edamam.com/search';
+const foodId = '7fd223ec';
+const foodKey = '817ee5945f6f33e30affd1cededc234a';
+const RESTAURANT_URL = 'https://api.foursquare.com/v2/venues/explore';
 
 //request to foursquare API
 function restaurantRequest(searchTerm, city, callback) {
@@ -21,14 +21,12 @@ function restaurantRequest(searchTerm, city, callback) {
             /* update API end point */
             url: RESTAURANT_URL,
             data: query,
-            dataType: "json",
+            dataType: 'json',
             /*set the call type GET / POST*/
-            type: "GET"
+            type: 'GET'
         })
         /* if the call is successful (status 200 OK) show results */
         .done(function (result) {
-            /* if the results are meaningful, we can just console.log them */
-            console.log(result);
             callbackRestaurant(result);
         })
         /* if the call is NOT successful show errors */
@@ -42,9 +40,8 @@ function restaurantRequest(searchTerm, city, callback) {
 
 //what to do with the foursquare data that's sent back
 function callbackRestaurant(data) {
-    console.log(data);
     let display = data.response.groups['0'].items.map((item, index) => renderRestaurants(item));
-    $("#restaurantResults").html(display);
+    $('#restaurantResults').html(display);
 }
 
 //how to show the data from foursquare on the page
@@ -52,19 +49,14 @@ function renderRestaurants(item) {
     let restaurantName = item.venue.name;
     let restaurantAddress = item.venue.location.address;
 
-    return `<div class="imageContainer resultsBackground product-image restaurantPicture">
-
-
-<h3>${restaurantName}</h3>
-<ul >
-<li id="addressBackground">Address: ${restaurantAddress || ""}</li>
-</ul>
-</div>`
+    return `<div class= "imageContainer resultsBackground">
+                <div class= "product-image restaurantPicture">
+                    <h3 class="restaurantName">${restaurantName}</h3>
+                    <p id="addressBackground">Address: ${restaurantAddress || ""}</p>
+                </div>
+            </div>`
 
 }
-
-
-
 
 //request to Edamam API
 function recipeRequest(searchTerm, callback) {
@@ -73,20 +65,18 @@ function recipeRequest(searchTerm, callback) {
         app_id: `${foodId}`,
         app_key: `${foodKey}`,
         to: 8,
-        image: ""
+        image: ''
     }
     let result = $.ajax({
             /* update API end point */
             url: FOOD_URL,
             data: query,
-            dataType: "json",
+            dataType: 'json',
             /*set the call type GET / POST*/
-            type: "GET"
+            type: 'GET'
         })
         /* if the call is successful (status 200 OK) show results */
         .done(function (result) {
-            /* if the results are meeningful, we can just console.log them */
-            console.log(result);
             callbackFood(result);
         })
         /* if the call is NOT successful show errors */
@@ -101,10 +91,9 @@ function recipeRequest(searchTerm, callback) {
 
 //what to do with the Edamam data that's sent back
 function callbackFood(data) {
-    console.log(data);
     let display = data.hits.map((item, index) => renderRecipes(item));
-    $("#recipeResults").html(display);
-    $("#recipeResults").html(display);
+    $('#recipeResults').html(display);
+    $('#recipeResults').html(display);
 };
 
 
@@ -131,14 +120,13 @@ function youtubeRequest(searchTerm, callback) {
             /* update API end point */
             url: YOUTUBE_URL,
             data: query,
-            dataType: "json",
+            dataType: 'json',
             /*set the call type GET / POST*/
-            type: "GET"
+            type: 'GET'
         })
         /* if the call is successful (status 200 OK) show results */
         .done(function (result) {
             /* if the results are meeningful, we can just console.log them */
-            console.log(result);
             callbackTube(result);
         })
         /* if the call is NOT successful show errors */
@@ -152,19 +140,18 @@ function youtubeRequest(searchTerm, callback) {
 
 //what to do with the data that is sent back from the YouTube request
 function callbackTube(data) {
-    console.log(data);
     let display = data.items.map((item, index) => render(item));
-    $("#youtubeResults").html(display);
+    $('#youtubeResults').html(display);
 
 }
 
 //how to show the data on the page
 function render(item) {
-    let href = "https://www.youtube.com/watch?v=" + item.id.videoId;
+    let href = 'https://www.youtube.com/watch?v=' + item.id.videoId;
     let title = item.snippet.title;
     let image = item.snippet.thumbnails.medium.url;
     return `<div class="imageContainer resultsBackground">
-<h3><a href= ${href} class="links">${title}</a></h3>
+<h3><a href= ${href} class='links'>${title}</a></h3>
 <div class = "product-image" style="background-image: url(${image})" alt=${title}></div>
 </div>`;
 }
@@ -174,22 +161,22 @@ function submitHandler() {
     $('form').on('submit', function (event) {
         event.preventDefault();
         let searchTerm = $('#search').val();
-        let city = $("#city").val();
-        $("#errorFood").html("");
-        $("#errorCity").html("");
+        let city = $('#city').val();
+        $('#errorFood').html('');
+        $('#errorCity').html('');
 
-        if (searchTerm === "") {
-            $("#errorFood").html("Oops, enter a food please");
-        } else if (city === "") {
-            $("#errorCity").html("Oops, enter a city");
+        if (searchTerm === '') {
+            $('#errorFood').html('Oops, enter a food please');
+        } else if (city === '') {
+            $('#errorCity').html('Oops, enter a city');
         } else {
-            $("#errorCity").html("");
-            $("#errorFood").html("");
-            $("#city").val("");
-            $('#search').val("");
-            $(".recipesHeader").html("<h2>Recipes</h2>");
-            $(".restaurantsHeader").html("<h2>Restaurants</h2>");
-            $(".youtubeHeader").html("<h2>YouTube</h2>");
+            $('#errorCity').html('');
+            $('#errorFood').html('');
+            $('#city').val('');
+            $('#search').val('');
+            $('.recipesHeader').html('<h2>Recipes</h2>');
+            $('.restaurantsHeader').html('<h2>Restaurants</h2>');
+            $('.youtubeHeader').html('<h2>YouTube</h2>');
             youtubeRequest(searchTerm, callbackTube);
             recipeRequest(searchTerm, callbackFood);
             restaurantRequest(searchTerm, city, callbackRestaurant);
